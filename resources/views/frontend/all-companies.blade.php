0a49e23a0593bb652eb6658fa6fbb825b8619084
Hsu Mon Lin <scm.hsumonlynn@gmail.com>
2021-08-30 09:53:23 +0630

    top page fix

--- resources/views/frontend/all-companies.blade.php
+++ resources/views/frontend/all-companies.blade.php
@@ -1,64 +1,28 @@
 @extends('frontend.frontend-layout.master')
 @section('frontend-content')
 
-    <div class="bg-primary p-5">
-        <h3 class="text-center text-uppercase text-white">Banner</h3>
-    </div>
+    
+<img src="images/job-main-banner.jpg" alt="" style="width:1349px; ">
+
     <div class="container py-5">
         <div class="section mb-5">
             <h3>All Companies</h3>
             <div class="row mt-3 mb-5">
+                @foreach($companies as $company)
                 <div class="col-md-3">
                     <div class="card p-3 shadow-md border border-dark">
-                        <img src="{{asset('images/wave_money.png')}}" alt="" class="rounded" height="120">
-                        <a href="{{url('/companies/1')}}"><h5 class="font-weight-bold mt-3 text-center" style="color: #3490DC">Company Name</h5></a>
+            
+                            <img src="{{asset('storage/company-logos/'.$company->logo)}}" alt="" class="rounded" height="120"> 
+    
+                        <a href="{{url('/companies/'.$company->id)}}"><h5 class="font-weight-bold mt-3 text-center" style="color: #3490DC">
+                            @foreach($company->users as $key => $user)
+                            {{$user->name}}
+                        @endforeach</h5></a>
                     </div>
                 </div>
-                <div class="col-md-3">
-                    <div class="card p-3 shadow-md border border-dark">
-                        <img src="{{asset('images/wave_money.png')}}" alt="" class="rounded" height="120">
-                        <h5 class="font-weight-bold mt-3 text-center" style="color: #3490DC">Company Name</h5>
-                    </div>
-                </div>
-                <div class="col-md-3">
-                    <div class="card p-3 shadow-md border border-dark">
-                        <img src="{{asset('images/wave_money.png')}}" alt="" class="rounded" height="120">
-                        <h5 class="font-weight-bold mt-3 text-center" style="color: #3490DC">Company Name</h5>
-                    </div>
-                </div>
-                <div class="col-md-3">
-                    <div class="card p-3 shadow-md border border-dark">
-                        <img src="{{asset('images/wave_money.png')}}" alt="" class="rounded" height="120">
-                        <h5 class="font-weight-bold mt-3 text-center" style="color: #3490DC">Company Name</h5>
-                    </div>
-                </div>
+                @endforeach   
             </div>
-            <div class="row mb-5">
-                <div class="col-md-3">
-                    <div class="card p-3 shadow-md border border-dark">
-                        <img src="{{asset('images/wave_money.png')}}" alt="" class="rounded" height="120">
-                        <a href="{{url('/companies/1')}}"><h5 class="font-weight-bold mt-3 text-center" style="color: #3490DC">Company Name</h5></a>
-                    </div>
-                </div>
-                <div class="col-md-3">
-                    <div class="card p-3 shadow-md border border-dark">
-                        <img src="{{asset('images/wave_money.png')}}" alt="" class="rounded" height="120">
-                        <h5 class="font-weight-bold mt-3 text-center" style="color: #3490DC">Company Name</h5>
-                    </div>
-                </div>
-                <div class="col-md-3">
-                    <div class="card p-3 shadow-md border border-dark">
-                        <img src="{{asset('images/wave_money.png')}}" alt="" class="rounded" height="120">
-                        <h5 class="font-weight-bold mt-3 text-center" style="color: #3490DC">Company Name</h5>
-                    </div>
-                </div>
-                <div class="col-md-3">
-                    <div class="card p-3 shadow-md border border-dark">
-                        <img src="{{asset('images/wave_money.png')}}" alt="" class="rounded" height="120">
-                        <h5 class="font-weight-bold mt-3 text-center" style="color: #3490DC">Company Name</h5>
-                    </div>
-                </div>
-            </div>
+            {{ $companies->links() }}   
         </div>
     </div>
 @endsection
--- resources/views/frontend/frontend-layout/master.blade.php
+++ resources/views/frontend/frontend-layout/master.blade.php
@@ -24,7 +24,7 @@
         <nav class="navbar navbar-expand-md navbar-dark sticky-top shadow-sm" style="background-color: #44749E">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    <h2>LOGO</h1>
+                    <img src="images/logo.png" width="100px" height="60px"alt="">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
