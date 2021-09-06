746adda4877e4613ef0bcd0059ce6d35e66d66ef
KhineZinThaw7 <scm.khinezinthaw@gmail.com>
2021-09-06 16:36:09 +0630

    review

--- app/Http/Controllers/Admin/JobController.php
+++ app/Http/Controllers/Admin/JobController.php
@@ -25,28 +25,6 @@ class JobController extends Controller
         $jobs = $this->jobService->index();
         return view('admin.jobs.index', compact('jobs'));
     }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
     /**
      * Display the specified resource.
      *
@@ -57,29 +35,6 @@ class JobController extends Controller
     {
         $job = Job::findOrFail($id);
         return view('admin.jobs.job-detail', compact('job'));
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
     }
 
     /**
