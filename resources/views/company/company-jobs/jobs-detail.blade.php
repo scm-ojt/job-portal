ed4d04c4cfebdb6c822d86008a7e2f69c3483362
Kyi Myat Noe Soe <scm.kyimyatnoesoe@gmail.com>
2021-09-07 08:47:26 +0630

    ok

--- resources/views/company/company-jobs/jobs-detail.blade.php
+++ resources/views/company/company-jobs/jobs-detail.blade.php
@@ -1,8 +1,7 @@
 @extends('company.company-layout.master')
 
 @section('company-content')
-<div class="container-fluid">
-    <a href="{{ route('jobs.index') }}" class="btn btn-primary float-right mr-5"><i class="fa fa-arrow-circle-left mr-1"></i>Back</a>
+<div class="container">
     <div class="row">
         <div class="col-md-12">
             <div class="card">
@@ -21,42 +20,44 @@
                     </ul>
                 </div>
             </div>
+            <div class="card">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="section mb-3">
+                            <h5 class="ml-5 mt-5">Job Requirements</h5>
+                            <ul>
+                                <li class="ml-5">{{$job->requirement}}</li>
+                            </ul>
+                        </div>
+                        <div class="section mb-3">
+                            <h5 class="ml-5">Working Hour</h5>
+                            <ul>
+                                <li class="ml-5">{{$job->working_hour}}</li>
+                            </ul>
+                        </div>
+                        <div class="section mb-3">
+                            <h5 class="ml-5">Salary</h5>
+                            <ul>
+                                <li class="ml-5">{{number_format($job->salary)}} MMK</li>
+                            </ul>
+                        </div>
+                        <div class="section mb-3">
+                            <h5 class="ml-5">Working Address</h5>
+                            <ul>
+                                <li class="ml-5">{{$job->address}}</li>
+                            </ul>
+                        </div>
+                        <div class="section mb-3">
+                            <h5 class="ml-5">Contact Information</h5>
+                            <ul>
+                                <li class="ml-5">{{$job->contact_information}}</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div> 
-    <div class="row">
-        <div class="col-md-12">
-            <hr>
-            <div class="section mb-3">
-                <h5>Job Requirements</h5>
-                <ul>
-                    <li>{{$job->requirement}}</li>
-                </ul>
-            </div>
-            <div class="section mb-3">
-                <h5>Working Hour</h5>
-                <ul>
-                    <li>{{$job->working_hour}}</li>
-                </ul>
-            </div>
-            <div class="section mb-3">
-                <h5>Salary</h5>
-                <ul>
-                    <li>{{$job->salary}} MMK</li>
-                </ul>
-            </div>
-            <div class="section mb-3">
-                <h5>Working Address</h5>
-                <ul>
-                    <li>{{$job->address}}</li>
-                </ul>
-            </div>
-            <div class="section mb-3">
-                <h5>Contact Information</h5>
-                <ul>
-                    <li>{{$job->contact_information}}</li>
-                </ul>
-            </div>
-        </div>
-    </div>
+   
 </div>
 @endsection
